8e9ab5d3b1af1d8d49a4f9334b86460f0575ae10
Roger Kwong <rkwong12@gmail.com>
2022-02-28 21:22:10 -0500

    Fix issue with imports

--- public/tuiter/ExploreScreen/ExploreScreen.js
+++ public/tuiter/ExploreScreen/ExploreScreen.js
@@ -1,10 +1,9 @@
-import NavigationSidebar from "../NavigationSidebar.js";
+import NavigationSidebar from "../NavigationSidebar/index.js";
 import ExploreComponent from "./ExploreComponent.js";
-import WhoToFollowList from "../WhoToFollowList.js";
+import WhoToFollowList from "../WhoToFollowList/index.js";
 
 (function ($) {
   $("#wd-explore").append(`
-        Hello world!
         <div class="row mt-2">
         <div class="col-2 col-md-2 col-lg-1 col-xl-2">
             ${NavigationSidebar()}
