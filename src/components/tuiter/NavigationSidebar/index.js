83e489f02b5eb037986a551a87d75736ed153b48
Roger Kwong <rkwong12@gmail.com>
2022-03-21 20:56:20 -0400

    Fix warnings

--- src/components/tuiter/NavigationSidebar/index.js
+++ src/components/tuiter/NavigationSidebar/index.js
@@ -7,31 +7,32 @@ const NavigationSidebar = ({ active = "explore" }) => {
           {" "}
           <i className="fab fa-twitter"></i>
         </a>
-        <a className="list-group-item">
+        <a className="list-group-item" href="/">
           <i className="fa fa-home"></i> Home
         </a>
         <a
           className={`list-group-item
-        ${active === "home" ? "active" : ""}`}
+        {active === "home" ? "active" : ""}`}
+          href="/"
         >
           <i className="fa fa-hashtag"></i> Explore
         </a>
-        <a className="list-group-item">
+        <a className="list-group-item" href="/">
           <i className="fa fa-bell"></i> Notifications
         </a>
-        <a className="list-group-item">
+        <a className="list-group-item" href="/">
           <i className="fa fa-envelope"></i> Messages
         </a>
-        <a className="list-group-item">
+        <a className="list-group-item" href="/">
           <i className="fa fa-bookmark"></i> Bookmarks
         </a>
-        <a className="list-group-item">
+        <a className="list-group-item" href="/">
           <i className="fa fa-list"></i> Lists
         </a>
-        <a className="list-group-item">
+        <a className="list-group-item" href="/">
           <i className="fa fa-user"></i> Profile
         </a>
-        <a className="list-group-item">
+        <a className="list-group-item" href="/">
           <i className="fa fa-ellipsis-h"></i> More
         </a>
       </div>
